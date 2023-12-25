aa64c66ccbb661041137188357fdbaa766154bee
esakki <esakki2104prsnl@gmail.com>
2023-12-25 23:27:48 +0530

    form validation using just-validate-plugin

--- Javascript/02-javascript-dom/01-form-validation/assets/js/main.js
+++ Javascript/02-javascript-dom/01-form-validation/assets/js/main.js
@@ -1,5 +1,82 @@
-const phoneInputField = document.querySelector("#phone");
-        const phoneInput = window.intlTelInput(phoneInputField, {
-            utilsScript:
-                "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
-        });
+import JustValidate from "just-validate";
+
+const appointmentFormEl = document.getElementById("appointmentForm");
+
+console.log(appointmentFormEl);
+const validateForm = new JustValidate(appointmentFormEl);
+
+validateForm.addField("#fullName",
+    [
+        { rule: "required" },
+        { rule: "minLength", value: 3 },
+        { rule: "maxLength", value: 15 },
+    ],
+    {
+        errorLabelCssClass: ["error-label"],
+    },
+);
+
+validateForm.addField('#email', [
+    {
+        rule: 'required',
+    },
+    {
+        rule: 'email',
+    },
+], {
+    errorLabelCssClass: ["error-label"],
+},
+);
+
+validateForm.addField('#mobile-no', [
+    {
+        rule: 'required',
+    },
+    {
+        rule: 'number',
+    },
+    {
+        rule: "maxNumber",
+        value: 10
+    },
+], {
+    errorLabelCssClass: ["error-label"],
+},
+);
+
+validateForm.addField('#person', [
+    {
+        rule: 'required',
+    },
+], {
+    errorLabelCssClass: ["error-label"],
+},
+)
+
+validateForm.addField("#time", [
+    {
+        rule: 'required',
+    },
+], {
+    errorLabelCssClass: ["error-label"],
+},
+)
+
+validateForm.addField("#date", [
+    {
+        rule: 'required',
+    },
+], {
+    errorLabelCssClass: ["error-label"],
+},
+)
+
+
+
+
+// Contact Code JS CDN
+const phoneInputField = document.querySelector("#mobile-no");
+const phoneInput = window.intlTelInput(phoneInputField, {
+    utilsScript:
+        "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
+});
