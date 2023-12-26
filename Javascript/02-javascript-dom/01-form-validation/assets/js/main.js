12ab6d7fe77c8008f722882a150fcd451533e4f8
esakki <esakki2104prsnl@gmail.com>
2023-12-26 23:15:09 +0530

    fix: adding dist file

--- Javascript/02-javascript-dom/01-form-validation/assets/js/main.js
+++ Javascript/02-javascript-dom/01-form-validation/assets/js/main.js
@@ -164,11 +164,6 @@ validateForm.onSuccess(() => {
     Appointment Date and Time : ${formObj.date} & ${formObj.time}
     `
 
-    //     const response = (` Full Name : ${formObj.name},
-    //   Appoinment to : ${formObj.person}
-    //   Appointment Date and Time : ${formObj.date} & ${formObj.time}
-    //   `);
-    // responseEl.innerText = response;
 
     appointmentFormEl.reset();
 });
@@ -184,3 +179,4 @@ const phoneInput = window.intlTelInput(phoneInputField, {
     utilsScript:
         "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
 });
+
