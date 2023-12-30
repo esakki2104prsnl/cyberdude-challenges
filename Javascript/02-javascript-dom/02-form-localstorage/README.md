d616d8a3c96587197129f26e6c3be12d54ed4b5c
esakki <esakki2104prsnl@gmail.com>
2023-12-30 17:25:43 +0530

    fix: make UI responsive & bulid dist file

---
+++ Javascript/02-javascript-dom/02-form-localstorage/README.md
@@ -0,0 +1,68 @@
+# User Data Form
+
+### Description
+
+CyberDude Network Appointment Form it is helps to CDN Family easier.
+
+Special Thanks to [Mr. Anbuselvan Rocky](https://instagram.com/anbuselvanrocky)
+
+## Table of Contents
+
+- [Project Title](#project-title)
+- [Description](#description)
+- [Demo Link](#demo-link)
+- [Internship](#internship)
+- [Technologies Used](#technologies-used)
+- [Features](#features)
+- [Installation](#installation)
+- [Contributing](#contributing)
+- [License](#license)
+- [Acknowledgments](#acknowledgments)
+
+## Demo Link
+
+<a target="_blank" href="https://esakki2104prsnl.github.io/cyberdude-challenges/Javascript/02-javascript-dom/01-form-validation/dist/">`Live Link🚀`</a>
+
+### Internship
+
+This internship is provided by [CyberDude Networks Pvt. Ltd.](https://youtube.com/cyberdudenetworks) as part of the 6-Month Free Internship program, a skill development initiative organized to enhance participants' skills. Mentoring was provided by [Mr. Anbuselvan Rocky](https://instagram.com/anbuselvanrocky). For more information, [you can contact CyberDude Networks here](https://cyberdudenetworks.com).
+
+### Technologies Used
+
+<a href="https://www.w3.org/html/" target="_blank"> <img src="https://w7.pngwing.com/pngs/201/90/png-transparent-logo-html-html5.png" alt="html5" width="40" height="40"/> </a>
+<a href="https://tailwindcss.com/" target="_blank"><img src="https://yt3.googleusercontent.com/ikv41jMTr1uHGdILrJhvbfVJcDt4oqhwApKX37TjAleF_cRPbF2W-waj7uMnS5JySvnlvAlTCg=s900-c-k-c0x00ffffff-no-rj" alt="tailwind" width="40" height="40"/> </a>
+<a href="https://www.w3.org/js/" target="_blank"><img src="https://cdn.pixabay.com/photo/2015/04/23/17/41/javascript-736400_1280.png" alt="javascript" width="40" height="40"/> </a>
+
+### Features
+
+- To help you understand my skills and information better, I kindly request you to take a look at my Profile.
+
+## Installation
+
+To install and run this project on your local machine, follow these steps:
+
+1. Clone the repository to your local machine:
+
+   ```bash
+   git clone https://github.com/esakki2104prsnl/cyberdude-challenges.git
+   ```
+
+## Contributing
+
+If you want to contribute to the project, please follow the guidelines outlined in the installation. You can also contribute and edit the design to make more Perfect.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
+
+### Acknowledgments
+
+- **Cyberdude Network Pvt Limited:** Thanks to [CyberDude Networks Pvt. Ltd.](https://youtube.com/cyberdudenetworks) opportunity and guidance.
+
+- **Mentor Name:** Special thanks to [Mr. Anbuselvan Rocky](https://instagram.com/anbuselvanrocky) for their valuable mentorship.
+
+---
+
+- 📫 How to Reach Me **https://www.linkedin.com/in/esakki-m/** 🚀
+
+---
--- Javascript/02-javascript-dom/02-form-localstorage/assets/js/main.js
+++ Javascript/02-javascript-dom/02-form-localstorage/assets/js/main.js
@@ -111,32 +111,100 @@ validate.onSuccess(() => {
         localStorage.setItem(localStorageKey, JSON.stringify(newDataArray));
     }
 
-    // Tomorrow start from Here 
-    const LSValue = localStorage.getItem("userDataValue")
-    console.log(LSValue[0]);
-
-    const fianlValue = localStorage.JSON.parse(LSValue)
-    console.log(fianlValue);
-    // const divEl = document.createElement("div")
-    // const oldData = exisitngDataArray.forEach(element => {
-    //     console.log(element);
-    // });
-    // const updateDataData = [...oldData]
-
-    // console.log(updateDataData);
-
-    // sectionEl.style.display = "block";
-
-
-    // const updateData = [...oldData]
-    // divEl.append(oldData)
-    // console.log(oldData);
-
     userDataEl.reset()
 });
 
 
-function updateUI() {
 
+// Delete Function
+
+function deleteData(userData) {
+    const confirmation = confirm(
+        `Do you want to delete '${userData["firstName"]}' record?`
+    );
+
+    if (confirmation) {
+        const existingUserData = localStorage.getItem(localStorageKey);
+
+        const userDataObj = JSON.parse(existingUserData);
+
+        // const otherRecords = courierDataObj.filter(
+        //   (courierReq) => courierReq.id != courierRequest["id"]
+        // );
+
+        // Push it to localstorage again, this time, i'm deleting that record (courierRequestId)
+        localStorage.setItem(localStorageKey, JSON.stringify(userDataObj));
+
+        deleteData();
+    }
 }
 
+
+userDataEl.addEventListener("submit", () => {
+
+    const fetchData = localStorage.getItem(localStorageKey)
+    const fechedDataArr = JSON.parse(fetchData)
+    // console.log(fechedDataFromLS);
+
+    const tableEl = document.querySelector("#dataTable");
+    console.log(tableEl);
+    const newFinalValue = [];
+
+    tableEl.innerHTML = ""
+
+
+    fechedDataArr.map((value) => {
+        const trEl = document.createElement("tr")
+        const tdEl1 = document.createElement("td")
+        const tdEl2 = document.createElement("td")
+        const tdEl3 = document.createElement("td")
+        const tdEl4 = document.createElement("td")
+        // const divEl = document.createElement("div")
+
+
+        tdEl1.textContent = value.username
+        tdEl1.classList.add("table-data")
+
+        const fullName = value.firstName + " " + value.lastName
+        tdEl2.textContent = fullName
+        tdEl2.classList.add("table-data")
+
+        const fullAddress = value.streetAddress + ", " + value.city + ", " + value.postalCode
+        tdEl3.textContent = fullAddress
+        tdEl3.classList.add("table-data")
+
+
+        // <div class="flex justify-center items-center">
+        //                 
+        //               </div>
+
+        // const tdAppendedDivEl = tdEl4.append(divEl)
+
+        tdEl4.innerHTML = `<div class="m-1 flex items-center justify-center ">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 32 32"
+          id="delete"
+        >
+          <path
+            d="M24.2,12.193,23.8,24.3a3.988,3.988,0,0,1-4,3.857H12.2a3.988,3.988,0,0,1-4-3.853L7.8,12.193a1,1,0,0,1,2-.066l.4,12.11a2,2,0,0,0,2,1.923h7.6a2,2,0,0,0,2-1.927l.4-12.106a1,1,0,0,1,2,.066Zm1.323-4.029a1,1,0,0,1-1,1H7.478a1,1,0,0,1,0-2h3.1a1.276,1.276,0,0,0,1.273-1.148,2.991,2.991,0,0,1,2.984-2.694h2.33a2.991,2.991,0,0,1,2.984,2.694,1.276,1.276,0,0,0,1.273,1.148h3.1A1,1,0,0,1,25.522,8.164Zm-11.936-1h4.828a3.3,3.3,0,0,1-.255-.944,1,1,0,0,0-.994-.9h-2.33a1,1,0,0,0-.994.9A3.3,3.3,0,0,1,13.586,7.164Zm1.007,15.151V13.8a1,1,0,0,0-2,0v8.519a1,1,0,0,0,2,0Zm4.814,0V13.8a1,1,0,0,0-2,0v8.519a1,1,0,0,0,2,0Z"
+          ></path>
+        </svg>
+        <p class="text-sm">Delete</p>
+        
+      </div>`
+
+        // tdEl4.textContent = ""
+        tdEl4.classList.add("table-data-delete")
+        tdEl4.addEventListener("click", (event) => {
+            deleteData(value)
+        })
+
+
+
+        trEl.append(tdEl1, tdEl2, tdEl3, tdEl4)
+        newFinalValue.push(trEl)
+    })
+
+    newFinalValue.forEach((el) => tableEl.append(el));
+})
