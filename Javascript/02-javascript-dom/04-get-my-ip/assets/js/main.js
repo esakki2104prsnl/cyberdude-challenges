40ed7a0667064c1b03ccb5329e7b1d5c0c4ca67d
esakki <esakki2104prsnl@gmail.com>
2024-01-08 07:45:12 +0530

    desc: 04-get-my-ip

---
+++ Javascript/02-javascript-dom/04-get-my-ip/assets/js/main.js
@@ -0,0 +1,13 @@
+const valueEl = document.getElementById("val");
+let ip = new XMLHttpRequest();
+
+let ipAddress = ip.onreadystatechange = function () {
+  if (this.readyState === 4 && this.status === 200) {
+    valueEl.textContent = ip.responseText
+  }
+};
+
+ip.open("GET", "https://ipv4.icanhazip.com/");
+ip.send();
+
+// Copy
